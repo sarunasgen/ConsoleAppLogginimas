dde5e42cdad928ee88db06e63781c8e3bc09b6f4
Sarunas <sarugen12@gmail.com>
2024-08-23 09:01:39 +0200

    Add project files.

---
+++ ConsoleAppLogginimas/CarsService.cs
@@ -0,0 +1,43 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppLogginimas
+{
+    public class CarsService
+    {
+        private List<string> Cars { get; set; }
+        public CarsService()
+        {
+            Cars = new List<string>();
+            Log.Information("Cars service started");
+        }
+        public void AddCar(string car)
+        {
+            if (!string.IsNullOrEmpty(car))
+            {
+                Cars.Add(car);
+                Log.Information("Car was added to the list: " + car);
+            }
+            else
+                Log.Error("Car was not added because it was null or empty value");
+
+        }
+        public void RemoveCar(string car)
+        {
+            Log.Debug("Car to delete " + car);
+            if(Cars.Remove(car))
+            {
+                Log.Information("Car was successfully removed");
+            }
+            else
+            {
+                Log.Error("Car was not removed, because it was not present in the list");
+            }
+        }
+    }
+}
---
+++ ConsoleAppLogginimas/Program.cs
@@ -0,0 +1,35 @@
+﻿using ConsoleAppLogginimas;
+using Serilog;
+
+var log = new LoggerConfiguration()
+    .WriteTo.File("logs/myapp.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+Log.Logger = log;
+
+CarsService cars = new CarsService();
+
+while(true)
+{
+    Console.WriteLine("1. Add Car");
+    Console.WriteLine("2. Remove Car");
+    string entry = Console.ReadLine();
+    switch(entry)
+    {
+        case "1":
+            Console.WriteLine("Write car name: ");
+            cars.AddCar(Console.ReadLine());
+            break;
+        case "2":
+            Console.WriteLine("Write car name you want to remove: ");
+            cars.RemoveCar(Console.ReadLine());
+            break;
+        case "0":
+            Log.Information("Application Stopped");
+            return;
+        default:
+            continue;
+    }
+}
+
+
